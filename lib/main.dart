d67abde8e3c3f4b5fa4769f05644e668c98c8b95
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-18 20:54:55 +0700

    chore: Update main.dart with preferred orientation settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:meals_app/src/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  );
   runApp(const App());
 }
