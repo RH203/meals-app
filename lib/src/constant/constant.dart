8ec3575c2c468434db21ce83305cc62ac0116ae5
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-18 17:13:56 +0700

    Add color constants to Constant class

---
+++ lib/src/constant/constant.dart
@@ -0,0 +1,9 @@
+class Constant {
+  
+  // Color palette
+  static const darkRed = "#B82626";
+  static const brightRed = "#E02222";
+  static const blackishGrey = "#1D1D1D";
+  static const warmWhite = "#FAFAFA";
+  static const white = "#FFFFFF";
+}
