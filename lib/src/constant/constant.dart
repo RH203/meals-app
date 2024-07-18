9a48e490f9c23685066586a24fa3e9e2ebdde4bd
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-18 20:55:07 +0700

    refactor: Update color constants in Constant class
    This commit updates the color constants in the `Constant` class to use the `Color` class from the `flutter/material.dart` package. The color values are now defined as `Color` objects instead of string hex values. This improves type safety and ensures consistent usage of colors throughout the codebase.

--- lib/src/constant/constant.dart
+++ lib/src/constant/constant.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
+
 class Constant {
-  
+  Constant._();
+
   // Color palette
-  static const darkRed = "#B82626";
-  static const brightRed = "#E02222";
-  static const blackishGrey = "#1D1D1D";
-  static const warmWhite = "#FAFAFA";
-  static const white = "#FFFFFF";
+  static const Color darkRed = Color(0xffB82626);
+  static const Color brightRed = Color(0xffE02222);
+  static const Color blackishGrey = Color(0xff1D1D1D);
+  static const Color warmWhite = Color(0xffFAFAFA);
+  static const Color white = Color(0xffFFFFFF);
 }
