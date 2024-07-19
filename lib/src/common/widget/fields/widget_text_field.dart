4a8373781d57854519bfe4120c3e319bd8e9f4e7
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-19 16:46:54 +0700

    feat: Add show/hide password functionality to WidgetTextField

--- lib/src/common/widget/fields/widget_text_field.dart
+++ lib/src/common/widget/fields/widget_text_field.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class WidgetTextField extends StatefulWidget {
-  const WidgetTextField({super.key});
+  const WidgetTextField({
+    super.key,
+    required this.label,
+    required this.hintText,
+    required this.validator,
+    required this.isPassword,
+  });
+
+  final String label, hintText;
+  final String Function(String?) validator;
+  final bool isPassword;
 
   @override
   State<WidgetTextField> createState() => _WidgetTextFieldState();
 }
 
 class _WidgetTextFieldState extends State<WidgetTextField> {
+  bool _show = true;
+
+  void _showPassword() {
+    setState(() {
+      _show = !_show;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      validator: widget.validator,
+      obscureText: _show,
       decoration: InputDecoration(
-        label: const Text("Email"),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                onPressed: _showPassword,
+                icon: _show
+                    ? const Icon(Icons.visibility)
+                    : const Icon(Icons.visibility_off),
+              )
+            : null,
+        label: Text(widget.label),
         labelStyle: Theme.of(context).textTheme.bodyLarge,
-        hintText: "example@gmail.com",
+        hintText: widget.hintText,
+        hintStyle: Theme.of(context).textTheme.bodyLarge,
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
         ),
