5be86ec1aea6496b375dfc86a14421c083c2c9f2
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-30 19:41:26 +0700

    feat: Add ServiceApi class for fetching category data

--- lib/src/service/service_api.dart
+++ lib/src/service/service_api.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:meals_app/src/features/homescreen/models/detail_meal_model.dart';
 import 'package:meals_app/src/features/homescreen/models/food_category_model.dart';
 import 'package:meals_app/src/utils/app_logger.dart';
 
@@ -11,20 +12,33 @@ class ServiceApi {
 
       if (response.statusCode == 200) {
         List<dynamic> categoriesJson = response.data['categories'];
-        List<FoodCategoryModel> categories =
-            categoriesJson.map((json) => FoodCategoryModel.fromJson(json)).toList();
+        List<FoodCategoryModel> categories = categoriesJson
+            .map((json) => FoodCategoryModel.fromJson(json))
+            .toList();
         // AppLogger.debug(categories);
         return categories;
       } else {
-        AppLogger.error("Failed to fetch: ${response.statusCode}");
-        throw Exception("Failed to fetch: ${response.statusCode}");
+        AppLogger.error(
+            "Failed to fetch [CATEGORY DATA]: ${response.statusCode}");
+        throw Exception(
+            "Exception failed status code [CATEGORY DATA]: ${response.statusCode}");
       }
     } on DioException catch (error) {
       AppLogger.error("Error DIO: $error");
       throw Exception("Error DIO: $error");
     } catch (error) {
-      AppLogger.error("Error when fetching Category data: $error");
-      throw Exception("Error when fetching Category data: $error");
+      AppLogger.error("Error when fetching [CATEGORY DATA]: $error");
+      throw Exception(
+          "Exception occured when fetching [CATEGORY DATA]: $error");
     }
   }
+
+  // Future<List<DetailMealModel>> fetchMealData(String url) async {
+  //   try {
+  //     final Response response = await _dio.get(url);
+  //   } catch (error) {
+  //     AppLogger.error("Error when fetching [MEAL DATA]: $error");
+  //     throw Exception("Exception occured when fetching [MEAL DATA]: $error");
+  //   }
+  // }
 }
