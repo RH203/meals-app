96fc1bddd7c52605f35f0bc60179ca9fe01a576c
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-30 19:05:13 +0700

    feat: Add lazy loading for images on page load

--- lib/src/features/homescreen/views/homescreen.dart
+++ lib/src/features/homescreen/views/homescreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/src/common/widget/fields/widget_text_field.dart';
+import 'package:go_router/go_router.dart';
 import 'package:meals_app/src/constant/constant.dart';
+import 'package:meals_app/src/features/homescreen/views/category_screen.dart';
 import 'package:meals_app/src/utils/app_logger.dart';
 
 class Homescreen extends StatefulWidget {
@@ -11,8 +12,6 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-  final TextEditingController _searchController = TextEditingController();
-
   String _validator(String? value) {
     // TODO: VALIDATOR
     return "";
@@ -26,36 +25,38 @@ class _HomescreenState extends State<Homescreen> {
           padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
           child: CustomScrollView(
             slivers: [
-              // Welcome widget
-              SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 180,
+              SliverAppBar(
+                floating: true,
+                backgroundColor: Constant.warmWhite,
+                surfaceTintColor: Constant.warmWhite,
+                elevation: 0,
+                actions: <Widget>[
+                  IconButton.outlined(
+                    onPressed: () => context.go('/search-page'),
+                    style: IconButton.styleFrom(
+                      padding: const EdgeInsets.all(5),
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Constant.darkRed,
+                    ),
+                    icon: const Icon(
+                      Icons.search,
+                      size: 30,
+                    ),
+                  )
+                ],
+                title: Expanded(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "Hello Raihan!",
-                            style: Theme.of(context).textTheme.headlineLarge,
-                          ),
-                          Text(
-                            "What would you like to make ?",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headlineSmall!
-                                .copyWith(fontSize: 16),
-                          ),
-                        ],
+                    children: [
+                      Text(
+                        "Hello Raihan!",
+                        style: Theme.of(context).textTheme.headlineLarge,
                       ),
-                      WidgetTextField(
-                        label: "Search",
-                        hintText: "Nasi padang",
-                        validator: _validator,
-                        isPassword: false,
-                        controller: _searchController,
+                      Text(
+                        "What would you like to make ?",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall!
+                            .copyWith(fontSize: 16),
                       ),
                     ],
                   ),
@@ -64,7 +65,7 @@ class _HomescreenState extends State<Homescreen> {
 
               SliverToBoxAdapter(
                 child: Container(
-                  margin: const EdgeInsets.only(top: 40),
+                  margin: const EdgeInsets.only(top: 20),
                   height: 50,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -94,36 +95,82 @@ class _HomescreenState extends State<Homescreen> {
                 ),
               ),
 
+              const CategoryScreen(),
+
+              //  recipe Of the day
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Recipe of the day",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontSize: 20),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          AppLogger.debug("View all pressed");
+                        },
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(0),
+                          backgroundColor: Constant.warmWhite,
+                        ),
+                        child: Text(
+                          "View All",
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
               SliverToBoxAdapter(
                 child: SizedBox(
-                  height: 120,
+                  height: 270,
                   child: GridView.builder(
                     itemCount: 20,
                     scrollDirection: Axis.horizontal,
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 1,
+                      mainAxisSpacing: 20,
                     ),
                     itemBuilder: (context, index) {
-                      AppLogger.debug("Item: $index");
                       return Column(
                         children: <Widget>[
-                          ClipOval(
-                            child: Image.asset(
-                              "assets/image/dummy_product/image.png",
-                              width: 70,
-                              height: 70,
-                              fit: BoxFit.cover,
+                          SizedBox(
+                            width: 270,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                              ),
+                              child: Image.asset(
+                                "assets/image/dummy_product/image.png",
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
                           Text(
-                            "Category Title",
-                            textAlign: TextAlign.center,
+                            "Dish Title",
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                          ),
+                          Text(
+                            "Lorem ipsum dolor sit amet consectetur. Urna nibh amet consectetur metus gravida. Phasellus erat tristique massa.",
                             style:
                                 Theme.of(context).textTheme.bodySmall!.copyWith(
                                       fontWeight: FontWeight.w600,
                                     ),
-                          ),
+                          )
                         ],
                       );
                     },
@@ -176,7 +223,168 @@ class _HomescreenState extends State<Homescreen> {
                       mainAxisSpacing: 20,
                     ),
                     itemBuilder: (context, index) {
-                      AppLogger.debug("Item: $index");
+                      return Column(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 270,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                              ),
+                              child: Image.asset(
+                                "assets/image/dummy_product/image.png",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "Dish Title",
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                          ),
+                          Text(
+                            "Lorem ipsum dolor sit amet consectetur. Urna nibh amet consectetur metus gravida. Phasellus erat tristique massa.",
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ),
+
+              // Trending recipe
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Trending Recipes",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontSize: 20),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          AppLogger.debug("View all pressed");
+                        },
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(0),
+                          backgroundColor: Constant.warmWhite,
+                        ),
+                        child: Text(
+                          "View All",
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 270,
+                  child: GridView.builder(
+                    itemCount: 20,
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 20,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 270,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                              ),
+                              child: Image.asset(
+                                "assets/image/dummy_product/image.png",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "Dish Title",
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                          ),
+                          Text(
+                            "Lorem ipsum dolor sit amet consectetur. Urna nibh amet consectetur metus gravida. Phasellus erat tristique massa.",
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                          )
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ),
+
+              // Trending recipe
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Trending Recipes",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontSize: 20),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          AppLogger.debug("View all pressed");
+                        },
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(0),
+                          backgroundColor: Constant.warmWhite,
+                        ),
+                        child: Text(
+                          "View All",
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 270,
+                  child: GridView.builder(
+                    itemCount: 20,
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 20,
+                    ),
+                    itemBuilder: (context, index) {
                       return Column(
                         children: <Widget>[
                           SizedBox(
