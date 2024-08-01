633bc5ad749ad82a2a9b1f7e582d4ad51c837c84
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-01 15:13:53 +0700

    feat: Add mealImage property to DetailMealModel
    This commit adds the `mealImage` property to the `DetailMealModel` class in the `detail_meal_model.dart` file. The `mealImage` property is a string that represents the URL of the meal image. This enhancement allows for displaying the meal image in the app.

--- lib/src/features/homescreen/models/detail_meal_model.dart
+++ lib/src/features/homescreen/models/detail_meal_model.dart
@@ -1,6 +1,11 @@
 class DetailMealModel {
   final int idMeal;
-  final int nameMeal, categoryMeal, mealRegion, mealInstructions, mealTags;
+  final String nameMeal,
+      categoryMeal,
+      mealRegion,
+      mealInstructions,
+      mealTags,
+      mealImage;
 
   DetailMealModel({
     required this.idMeal,
@@ -9,15 +14,18 @@ class DetailMealModel {
     required this.mealRegion,
     required this.mealInstructions,
     required this.mealTags,
+    required this.mealImage,
   });
 
   factory DetailMealModel.fromJson(Map<String, dynamic> json) {
     return DetailMealModel(
-        idMeal: int.parse(json["idMeal"]),
-        nameMeal: json["strMeal"],
-        categoryMeal: json["strCategory"],
-        mealRegion: json["strArea"],
-        mealInstructions: json["strInstructions"],
-        mealTags: json["strTags"]);
+      idMeal: int.parse(json["idMeal"]),
+      nameMeal: json["strMeal"] as String,
+      categoryMeal: json["strCategory"] as String,
+      mealRegion: json["strArea"] as String,
+      mealInstructions: json["strInstructions"] as String,
+      mealImage: json["strMealThumb"] as String,
+      mealTags: json["strTags"] ?? "null",
+    );
   }
 }
