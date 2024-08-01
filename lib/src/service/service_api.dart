c39160daeed27223cb3fb5a74c76311dce3b40ec
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-01 15:14:16 +0700

    feat: Update ServiceApi to fetch meal data for multiple days
    This commit updates the `ServiceApi` class in the `service_api.dart` file to fetch meal data for multiple days. The `fetchMealData` method now fetches meal data for 10 days by making multiple API requests. The fetched data is stored in a list of lists of `DetailMealModel` objects. This feature enhancement improves the app's ability to display a variety of meal options over multiple days.

--- lib/src/service/service_api.dart
+++ lib/src/service/service_api.dart
@@ -15,7 +15,7 @@ class ServiceApi {
         List<FoodCategoryModel> categories = categoriesJson
             .map((json) => FoodCategoryModel.fromJson(json))
             .toList();
-        // AppLogger.debug(categories);
+        // AppLogger.debug("Fetched categories data successfully: $categories");
         return categories;
       } else {
         AppLogger.error(
@@ -24,21 +24,61 @@ class ServiceApi {
             "Exception failed status code [CATEGORY DATA]: ${response.statusCode}");
       }
     } on DioException catch (error) {
-      AppLogger.error("Error DIO: $error");
-      throw Exception("Error DIO: $error");
-    } catch (error) {
-      AppLogger.error("Error when fetching [CATEGORY DATA]: $error");
+      String? errorMessage = error.message;
+      int? statusCode = error.response?.statusCode;
+      String statusMessage = error.response?.statusMessage ?? "Unknown error";
+
+      AppLogger.error(
+          "DioException: Failed to fetch category data. URL: $url, Status Code: $statusCode, Status Message: $statusMessage, Error Message: $errorMessage");
+
       throw Exception(
-          "Exception occured when fetching [CATEGORY DATA]: $error");
+          "Failed to fetch category data from $url. Status Code: $statusCode, Error Message: $errorMessage");
+    } catch (error) {
+      AppLogger.error(
+          "Exception: An error occurred while fetching category data. URL: $url, Error: $error");
+      throw Exception(
+          "An unexpected error occurred while fetching category data from $url. Error: $error");
     }
   }
 
-  // Future<List<DetailMealModel>> fetchMealData(String url) async {
-  //   try {
-  //     final Response response = await _dio.get(url);
-  //   } catch (error) {
-  //     AppLogger.error("Error when fetching [MEAL DATA]: $error");
-  //     throw Exception("Exception occured when fetching [MEAL DATA]: $error");
-  //   }
-  // }
+  Future<List<List<DetailMealModel>>> fetchMealData(String url) async {
+    try {
+      List<List<DetailMealModel>> allMeals = [];
+      for (int i = 0; i < 10; i++) {
+        final Response response = await _dio.get(url);
+
+        if (response.statusCode == 200) {
+          List<dynamic> mealJson = response.data["meals"];
+          List<DetailMealModel> meals =
+              mealJson.map((json) => DetailMealModel.fromJson(json)).toList();
+          allMeals.add(meals);
+        } else {
+          String statusMessage = response.statusMessage ?? "Unknown error";
+
+          AppLogger.error(
+              "Failed to fetch meal data. URL: $url, Status Code: ${response.statusCode}, Status Message: $statusMessage");
+
+          throw Exception(
+              "Failed to fetch meal data. Server responded with status code ${response.statusCode}: $statusMessage");
+        }
+      }
+
+      return allMeals;
+    } on DioException catch (error) {
+      String? errorMessage = error.message;
+      int? statusCode = error.response?.statusCode;
+      String statusMessage = error.response?.statusMessage ?? "Unknown error";
+
+      AppLogger.error(
+          "DioException: Failed to fetch meal data. URL: $url, Status Code: $statusCode, Status Message: $statusMessage, Error Message: $errorMessage");
+
+      throw Exception(
+          "Failed to fetch meal data from $url. Status Code: $statusCode, Error Message: $errorMessage");
+    } catch (error) {
+      AppLogger.error(
+          "Exception: An error occurred while fetching meal data. URL: $url, Error: $error");
+      throw Exception(
+          "An unexpected error occurred while fetching meal data from $url. Error: $error");
+    }
+  }
 }
