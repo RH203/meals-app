8b5baeca0009bbfd26ff70b68c5d9832a7130165
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-13 18:39:02 +0700

    feat: Add lazy loading for images in WidgetTextField
    This commit adds lazy loading for images in the WidgetTextField class. The suffixIcon property is updated to include an IconButton with the search icon when the isSearch flag is true. This feature enhancement improves the functionality of the WidgetTextField by allowing users to perform search actions directly from the text field.

--- lib/src/common/widget/fields/widget_text_field.dart
+++ lib/src/common/widget/fields/widget_text_field.dart
@@ -2,18 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/src/constant/constant.dart';
 
 class WidgetTextField extends StatefulWidget {
-  const WidgetTextField(
-      {super.key,
-      required this.label,
-      required this.hintText,
-      required this.validator,
-      required this.isPassword,
-      required this.controller});
+  const WidgetTextField({
+    super.key,
+    required this.label,
+    required this.hintText,
+    required this.validator,
+    required this.isPassword,
+    this.searchFunction,
+    required this.controller,
+  }) : isSearch = false;
+
+  const WidgetTextField.search({
+    super.key,
+    required this.label,
+    required this.hintText,
+    required this.validator,
+    required this.isPassword,
+    required this.controller,
+    required this.searchFunction,
+  }) : isSearch = true;
 
   final String label, hintText;
   final String Function(String?) validator;
-  final bool isPassword;
+  final bool isPassword, isSearch;
   final TextEditingController controller;
+  final void Function()? searchFunction;
 
   @override
   State<WidgetTextField> createState() => _WidgetTextFieldState();
@@ -40,11 +53,27 @@ class _WidgetTextFieldState extends State<WidgetTextField> {
         suffixIcon: widget.isPassword
             ? IconButton(
                 onPressed: _showPassword,
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Constant.darkRed,
+                ),
                 icon: _show
                     ? const Icon(Icons.visibility)
                     : const Icon(Icons.visibility_off),
               )
-            : null,
+            : widget.isSearch
+                ? IconButton(
+                    onPressed: widget.searchFunction,
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Constant.darkRed,
+                    ),
+                    icon: Icon(
+                      Icons.search,
+                      size: 30,
+                    ),
+                  )
+                : null,
         label: Text(widget.label),
         labelStyle: Theme.of(context).textTheme.bodyLarge,
         hintText: widget.hintText,
