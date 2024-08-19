9303949c0b2243b397235076eeae44f1a1555673
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-19 20:41:17 +0700

    feat: Add lazy loading for images in HomescreenMenu

--- lib/src/features/homescreen/views/homescreen.dart
+++ lib/src/features/homescreen/views/homescreen.dart
@@ -15,123 +15,121 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
-          child: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                floating: true,
-                backgroundColor: Constant.warmWhite,
-                surfaceTintColor: Constant.warmWhite,
-                elevation: 0,
-                leading: IconButton.outlined(
-                  onPressed: () {},
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              backgroundColor: Constant.warmWhite,
+              surfaceTintColor: Constant.warmWhite,
+              elevation: 0,
+              leading: IconButton.outlined(
+                onPressed: () {},
+                style: IconButton.styleFrom(
+                  padding: const EdgeInsets.all(5),
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Constant.darkRed,
+                ),
+                icon: const Icon(Icons.person),
+              ),
+              actions: <Widget>[
+                IconButton.outlined(
+                  onPressed: () => context.go('/search-page'),
                   style: IconButton.styleFrom(
                     padding: const EdgeInsets.all(5),
                     backgroundColor: Colors.transparent,
                     foregroundColor: Constant.darkRed,
                   ),
-                  icon: const Icon(Icons.person),
-                ),
-                actions: <Widget>[
-                  IconButton.outlined(
-                    onPressed: () => context.go('/search-page'),
-                    style: IconButton.styleFrom(
-                      padding: const EdgeInsets.all(5),
-                      backgroundColor: Colors.transparent,
-                      foregroundColor: Constant.darkRed,
-                    ),
-                    icon: const Icon(
-                      Icons.search,
-                      size: 30,
-                    ),
-                  )
+                  icon: const Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                )
+              ],
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hello Raihan!",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  Text(
+                    "What would you like to make ?",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(fontSize: 16),
+                  ),
                 ],
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.only(top: 20),
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
                     Text(
-                      "Hello Raihan!",
-                      style: Theme.of(context).textTheme.headlineLarge,
-                    ),
-                    Text(
-                      "What would you like to make ?",
+                      "Category",
                       style: Theme.of(context)
                           .textTheme
-                          .headlineSmall!
-                          .copyWith(fontSize: 16),
+                          .bodyLarge!
+                          .copyWith(fontSize: 20),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        AppLogger.debug("View all pressed");
+                      },
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(0),
+                        backgroundColor: Constant.warmWhite,
+                      ),
+                      child: Text(
+                        "View All",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                   ],
                 ),
               ),
-              SliverToBoxAdapter(
-                child: Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Category",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyLarge!
-                            .copyWith(fontSize: 20),
+            ),
+            const CategoryScreen(),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.only(top: 10),
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Recipe of the day",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyLarge!
+                          .copyWith(fontSize: 20),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        AppLogger.debug("View all pressed");
+                      },
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(0),
+                        backgroundColor: Constant.warmWhite,
                       ),
-                      TextButton(
-                        onPressed: () {
-                          AppLogger.debug("View all pressed");
-                        },
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.all(0),
-                          backgroundColor: Constant.warmWhite,
-                        ),
-                        child: Text(
-                          "View All",
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
+                      child: Text(
+                        "View All",
+                        style: Theme.of(context).textTheme.bodyMedium,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-              const CategoryScreen(),
-              SliverToBoxAdapter(
-                child: Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Recipe of the day",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyLarge!
-                            .copyWith(fontSize: 20),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          AppLogger.debug("View all pressed");
-                        },
-                        style: TextButton.styleFrom(
-                          padding: const EdgeInsets.all(0),
-                          backgroundColor: Constant.warmWhite,
-                        ),
-                        child: Text(
-                          "View All",
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              HomescreenMenu()
-            ],
-          ),
+            ),
+            HomescreenMenu()
+          ],
         ),
       ),
     );
