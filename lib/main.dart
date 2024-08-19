b4b7a69d2d3cf0f2db567f68eb2e431bd88a6589
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-08-19 20:41:09 +0700

    feat: Add lazy loading for images in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:meals_app/src/app.dart';
+import 'package:meals_app/src/injection/injection.dart';
 
 void main() {
+  setup();
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   SystemChrome.setPreferredOrientations(
     [
       DeviceOrientation.portraitUp,
